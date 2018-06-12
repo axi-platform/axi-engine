bcaf1ff23d300c747a25cd516c3aa3d9e4f5122b
Pmc Dev <phoomparin@gmail.com>
2018-06-13 01:39:47 +0700

    :sparkles: Added Manual Acknowledgements

--- src/core/queue.js
+++ src/core/queue.js
@@ -9,7 +9,7 @@ export async function send(exchange, key, data) {
   const ch = await conn.createChannel()
   const message = msgpack.pack(data)
 
-  await ch.assertExchange(exchange, 'topic', {durable: false})
+  ch.assertExchange(exchange, 'topic', {durable: false})
 
   return ch.publish(exchange, key, message)
 }
@@ -19,7 +19,8 @@ export async function consume(exchange, key, handle) {
   const conn = await open
   const ch = await conn.createChannel()
 
-  await ch.assertExchange(exchange, 'topic', {durable: false})
+  ch.assertExchange(exchange, 'topic', {durable: false})
+  ch.prefetch(1)
 
   const {queue} = await ch.assertQueue('', {exclusive: true})
   console.log('[>] Queue name:', queue)
@@ -31,7 +32,9 @@ export async function consume(exchange, key, handle) {
     const data = msgpack.unpack(content)
 
     await handle(data, meta.fields.routingKey, meta)
+
+    return ch.ack(message)
   }
 
-  return ch.consume(queue, handler, {noAck: true})
+  return ch.consume(queue, handler)
 }
